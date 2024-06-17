ede1984a3d9d4ffb7d955811a3ed8a84aa2c5e02
CristianDavidEC <cescobar@stt.com.co>
2024-06-16 19:11:27 -0500

    change test

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from main_delete_edge import main_delete_edge
 
 if __name__ == '__main__':
     data_to_process = {
-        'file': 'data/tablex5.json',
-        'future': 'ABCDE',
-        'current': 'ABCDE',
-        'state': '10001',
-        'channels': 'ABCDE',
-        'method': 'partition' # partition | delete_edges |
+        'file': 'data/tablex6.json',
+        'future': 'ABCDEF',
+        'current': 'ABCD',
+        'state': '1111',
+        'channels': 'ABCDEF',
+        'method': 'delete_edges' # partition | delete_edges |
     }
 
     if data_to_process['method'] == 'partition':
