1c0a0d0d3a2535994fce16951c9cabe88510348e
CristianDavidEC <cescobar@stt.com.co>
2024-06-16 20:12:30 -0500

    fix

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from execute import execute
 
 if __name__ == '__main__':
     data_to_process = {
-        'file': 'data/tablex6.json',
+        'file': 'data/tablex5.json',
         'future': 'ABCDEF',
         'current': 'ABCD',
         'state': '1111',
         'channels': 'ABCDEF',
-        'method': 'delete_edges' # partition | delete_edges |
+        'method': 'delete_edges' # partition | delete_edges
     }
 
     execute(data_to_process)
