4818029301967751a67bde5c969269b9351ea698
CristianDavidEC <cescobar@stt.com.co>
2024-06-19 02:45:36 -0500

    changes test

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from execute import execute
 
 if __name__ == '__main__':
     data_to_process = {
-        'file': 'data/tablex6.json',
-        'future': 'ABC',
-        'current': 'AC',
-        'state': '10',
+        'file': 'data/tablex6-ramdom.json',
+        'future': 'ABCD',
+        'current': 'ABCD',
+        'state': '1001',
         'channels': 'ABCDEF',#100010
         'method': 'delete_edges' # partition | delete_edges
     }
